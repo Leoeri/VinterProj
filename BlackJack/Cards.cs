9e3eb6be2015ea3093be7d3e19e757b77a10039e
leoeri <leo.eriksson@elev.ga.ntig.se>
2021-12-06 09:30:42 +0100

    No idea

--- BlackJack/Cards.cs
+++ BlackJack/Cards.cs
@@ -6,6 +6,7 @@ public class Cards
     Random generator = new Random();
     List<int> number = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14 };
     List<string> shape = new List<string> { "Hearts", "Diamonds", "Clovers", "Spades" };
+    Dictionary<string, int> myCards = new Dictionary<string, int>();
 
     public void newCard()
     {
@@ -14,5 +15,7 @@ public class Cards
         int whatShape = generator.Next(0, 3);
 
         System.Console.WriteLine(number[whatNumber] + " of " + shape[whatShape]);
+
     }
+
 }
