8b98ebc52304d94a7c80158d612746f4151df2c3
leoeri <leo.eriksson@elev.ga.ntig.se>
2021-12-06 09:31:00 +0100

    no idea

--- BlackJack/Program.cs
+++ BlackJack/Program.cs
@@ -1,10 +1,37 @@
 ﻿using System;
 
 Cards card = new Cards();
-
+bool hasMoney = true;
+bool round = true;
+bool roundAns = false;
 card.newCard();
 
+while (hasMoney)
+{
+    while (round)
+    {
+        while (!roundAns)
+        {
+            System.Console.WriteLine("What do you want to do \n1. Hit \n2. Stand");
+            string answer = Console.ReadLine();
+            switch (answer)
+            {
+                case "1":
+                    card.newCard();
+                    break;
+                case "2":
+                    round = false;
+                    break;
+                default:
+                    System.Console.WriteLine("Choose between 1 or 2");
+                    answer = Console.ReadLine();
+                    break;
 
+
+            }
+        }
+    }
+}
 
 
 Console.ReadLine();
